65e2c4fe27973cdacdd7a8ce35e414cbafa6166c
Jessica Watts <jessperr182@hotmail.com>
2020-09-10 23:03:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,20 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
+  indonesia = ["Bali", "Lombok", "Java", "Sumatra"]
   
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(array,element)
+  array.push(element)
   
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(array,element)
+  array.unshift(element)
+
 end
 
 def remove_element_from_end_of_array(array)
